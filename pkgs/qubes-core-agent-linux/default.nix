2e1fc64d5f70310adeae080d9c984e6a66c1d110
eV <ev@7pr.xyz>
2025-12-13 10:05:43 +0000

    qubes-core-agent-linux: fix missing arg passthrough

--- pkgs/qubes-core-agent-linux/default.nix
+++ pkgs/qubes-core-agent-linux/default.nix
@@ -1,5 +1,9 @@
-{callPackage}:
+{
+  callPackage,
+  enableNetworking ? false,
+}:
 callPackage ./generic.nix {
   version = "4.2.44";
   hash = "sha256-k3zBx3ND7sXPo/SC2VEicA2RzPom1dWkbOjhKU04DYs=";
+  inherit enableNetworking;
 }
